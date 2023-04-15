a65a6289e61c595bfa05aab9706e6d1263542de8
Vatkovski <yulian.vatkovski@gmail.com>
2023-04-15 09:25:46 +0300

    Add files via upload

---
+++ Programming Basics JavaScript/3.1. Conditional Statements Advanced - Exercise/06. Operations Between Numbers.js
@@ -0,0 +1,57 @@
+function newHome(input) {
+    let num1 = Number(input[0]);
+    let num2 = Number(input[1]);
+    let operator = (input[2]);
+
+    switch (operator) {
+        case "+":
+            result = num1 + num2;
+            if (result % 2 === 0) {
+                console.log(`${num1} ${operator} ${num2} = ${result} - even`);
+            } else {
+                console.log(`${num1} ${operator} ${num2} = ${result} - odd`);
+            }
+            break;
+        case "-":
+            result = num1 - num2;
+            if (result % 2 === 0) {
+                console.log(`${num1} ${operator} ${num2} = ${result} - even`);
+            } else {
+                console.log(`${num1} ${operator} ${num2} = ${result} - odd`);
+            }
+            break;
+        case "*":
+            result = num1 * num2;
+            if (result % 2 === 0) {
+                console.log(`${num1} ${operator} ${num2} = ${result} - even`);
+            } else {
+                console.log(`${num1} ${operator} ${num2} = ${result} - odd`);
+            }
+            break;
+        case "/":
+            if (num2 === 0) {
+                console.log(`Cannot divide ${num1} by zero`);
+            } else {
+                result = num1 / num2
+                console.log(`${num1} ${operator} ${num2} = ${result.toFixed(2)}`);
+            }
+            break;
+        case "%":
+            if (num2 === 0) {
+                console.log(`Cannot divide ${num1} by zero`);
+            } else {
+                result = num1 % num2
+                console.log(`${num1} ${operator} ${num2} = ${result}`);
+            }
+            break;
+    }
+}
+
+newHome(["123",
+"12",
+"/"])
+
+
+
+
+
