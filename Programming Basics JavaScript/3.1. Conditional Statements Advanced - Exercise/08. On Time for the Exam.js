6818bafcb4aaba8b99c610c3f106055419bf261a
Vatkovski <yulian.vatkovski@gmail.com>
2023-04-17 12:17:51 +0300

    Add files via upload

---
+++ Programming Basics JavaScript/3.1. Conditional Statements Advanced - Exercise/08. On Time for the Exam.js
@@ -0,0 +1,13 @@
+function onTimeForTheExam(input) {
+    let examHour = Number(input[0]);
+    let examMinute = Number(input[1]);
+    let arrivalHour = Number(input[2]);
+    let arrivalMinute = Number(input[3]);
+
+    
+
+}
+onTimeForTheExam(["9",
+"30",
+"9",
+"50"]);
