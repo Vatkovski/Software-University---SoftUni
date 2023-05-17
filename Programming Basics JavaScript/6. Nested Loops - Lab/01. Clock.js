6876431c250c1c92081a4eb93ee2e90b0d7fd939
Vatkovski <yulian.vatkovski@gmail.com>
2023-05-17 16:52:04 +0300

    Add files via upload

---
+++ Programming Basics JavaScript/6. Nested Loops - Lab/01. Clock.js
@@ -0,0 +1,12 @@
+function clock(input) {
+
+    for (let i = 0; i <= 23; i++) {
+        for (let min = 0; min <= 59; min++) {
+            console.log(`${i}:${min}`);
+
+        }
+    }
+}
+
+
+clock()
