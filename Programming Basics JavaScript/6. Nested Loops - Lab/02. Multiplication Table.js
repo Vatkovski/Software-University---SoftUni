98e5c7e0b6be62dbc3fe04c760086f9d9fa19536
Vatkovski <yulian.vatkovski@gmail.com>
2023-05-18 13:39:16 +0300

    Add files via upload

---
+++ Programming Basics JavaScript/6. Nested Loops - Lab/02. Multiplication Table.js
@@ -0,0 +1,12 @@
+function  multiplicationTable (input) {
+
+    for (let num1 = 1; num1 <= 10; num1++) {
+        for (let num2 = 1; num2 <= 10; num2++) {
+            let result = num1 * num2;
+            console.log(`${num1} * ${num2} = ${result}`);
+            
+        }
+        
+    }
+    }
+    multiplicationTable()
