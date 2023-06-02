b287c8aa6bc37e8eb6e2989d7a81cc19fbb80eb5
Vatkovski <yulian.vatkovski@gmail.com>
2023-06-02 11:07:05 +0300

    Add files via upload

---
+++ 2. JS Fundamentals/1. Basic Syntax, Conditional Statements and Loops - Lab/04. Foreign Languages.js
@@ -0,0 +1,25 @@
+function foreignLanguages(country) {
+    switch (country) {
+      case "England":
+        console.log("English");
+        break;
+      case "USA":
+        console.log("English");
+        break;
+      case "Spain":
+        console.log("Spanish");
+        break;
+      case "Argentina":
+        console.log("Spanish");
+        break;
+      case "Mexico":
+        console.log("Spanish");
+        break;
+      default:
+        console.log("unknown");
+        break;
+    }
+  }
+  
+  foreignLanguages("Bulgaria");
+  
