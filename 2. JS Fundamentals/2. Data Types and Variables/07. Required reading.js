963a9ce15ba0f6dff68aecfe5f8cea0e53fe6da1
Vatkovski <yulian.vatkovski@gmail.com>
2023-06-26 14:34:20 +0300

    Add files via upload

---
+++ 2. JS Fundamentals/2. Data Types and Variables/07. Required reading.js
@@ -0,0 +1,9 @@
+function requiredReading(numPages, pagesRead, daysToRead) {
+    let hoursPerDay = (numPages / pagesRead) / daysToRead;
+    console.log(hoursPerDay);
+} 
+
+requiredReading(212,
+20 ,
+2 
+);
