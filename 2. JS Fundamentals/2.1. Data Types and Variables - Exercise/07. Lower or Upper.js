e273e812853d67f47c0ddeb65455355c05171bd3
Vatkovski <yulian.vatkovski@gmail.com>
2023-07-05 12:21:05 +0300

    Add files via upload

---
+++ 2. JS Fundamentals/2.1. Data Types and Variables - Exercise/07. Lower or Upper.js
@@ -0,0 +1,9 @@
+function lowerOrUpper(char) {
+    if (char >= "A" && char <= "Z") {
+        console.log('upper-case');
+    } else {
+        console.log('lower-case');
+    }
+} 
+   
+lowerOrUpper('L');
