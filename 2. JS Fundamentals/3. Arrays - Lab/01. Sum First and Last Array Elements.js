27692a7a6c85eef587e2ce5f1156b3d63aef02b4
Vatkovski <yulian.vatkovski@gmail.com>
2023-07-09 10:10:20 +0300

    Add files via upload

---
+++ 2. JS Fundamentals/3. Arrays - Lab/01. Sum First and Last Array Elements.js
@@ -0,0 +1,7 @@
+function sumFirstAndLastArrayElements(input) {
+    let firstElement = Number(input[0]);
+    let lastElement = Number(input[input.length-1]);
+    console.log(firstElement + lastElement);
+ }
+ 
+ sumFirstAndLastArrayElements([20, 30, 40]);
