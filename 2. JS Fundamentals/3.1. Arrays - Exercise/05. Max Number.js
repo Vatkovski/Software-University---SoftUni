9ab5e5634eaf9ab13b9e66a46857883d6eb5ead3
Vatkovski <yulian.vatkovski@gmail.com>
2023-07-15 09:18:58 +0300

    Add files via upload

---
+++ 2. JS Fundamentals/3.1. Arrays - Exercise/05. Max Number.js
@@ -0,0 +1,18 @@
+function findTopIntegers(arr) {
+    let topIntegers = [];
+    for (let i = 0; i < arr.length; i++) {
+        let isTopInteger = true;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] <= arr[j]) {
+                isTopInteger = false;
+                break;
+            }
+        }
+        if (isTopInteger) {
+            topIntegers.push(arr[i]);
+        }
+    }
+    console.log(topIntegers);
+}
+
+findTopIntegers([1, 4, 3, 2]);
