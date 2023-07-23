bfe339e93f61a00bfa5f2d3975f9c0942182ae9e
Vatkovski <yulian.vatkovski@gmail.com>
2023-07-23 11:39:00 +0300

    Add files via upload

---
+++ 2. JS Fundamentals/4.1. Functions - Exercise/01. Smallest of Three Numbers.js
@@ -0,0 +1,8 @@
+function smallestOfThreeNumbers(num1, num2, num3) {    
+    let smallest = Math.min(num1, num2, num3);
+    return smallest;
+}
+
+console.log(smallestOfThreeNumbers(2,
+    5,
+    3));
