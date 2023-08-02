f143f24735caa32e68afd51fe24a6a92b4239c3d
Vatkovski <yulian.vatkovski@gmail.com>
2023-08-02 10:02:29 +0300

    Add files via upload

---
+++ 2. JS Fundamentals/4.1. Functions - Exercise/10.1. Factorial Division.js
@@ -0,0 +1,14 @@
+function factorialDivision(num1, num2) {
+    let factorial1 = 1;
+    let factorial2 = 1;
+    for (let i = 1; i <= num1; i++) {
+        factorial1 *= i;
+    }
+    for (let j = 1; j <= num2; j++) {
+        factorial2 *= j;
+    }
+    let result = (factorial1 / factorial2).toFixed(2);
+    return result;
+}
+
+console.log(factorialDivision(5, 2)); 
