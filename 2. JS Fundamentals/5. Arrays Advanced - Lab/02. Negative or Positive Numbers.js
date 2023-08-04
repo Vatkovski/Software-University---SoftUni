4001d45ac2fc1e6321d2c5f8b784790e181e8883
Vatkovski <yulian.vatkovski@gmail.com>
2023-08-04 09:46:57 +0300

    Add files via upload

---
+++ 2. JS Fundamentals/5. Arrays Advanced - Lab/02. Negative or Positive Numbers.js
@@ -0,0 +1,15 @@
+function negativeOrPositiveNumbers(arr) {    
+    let result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < 0) {
+            result.unshift(arr[i]);
+        } else {
+            result.push(arr[i]);
+        }        
+    }
+    
+    console.log(result.join('\n'));
+}
+
+negativeOrPositiveNumbers(['7', '-2', '8', '9']);
