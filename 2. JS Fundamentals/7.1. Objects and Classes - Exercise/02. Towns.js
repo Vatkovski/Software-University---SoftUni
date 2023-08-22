279e518f9e71312686d7a655d9ea520115b0d5c3
Vatkovski <yulian.vatkovski@gmail.com>
2023-08-22 15:26:32 +0300

    Add files via upload

---
+++ 2. JS Fundamentals/7.1. Objects and Classes - Exercise/02. Towns.js
@@ -0,0 +1,17 @@
+function towns(input) {
+    
+    for (let i = 0; i < input.length; i++) {
+        let [name, latitude, longitude] = input[i].split(' | ');
+        let currentTown = {
+            town: name,
+            latitude: Number(latitude).toFixed(2),
+            longitude: Number(longitude).toFixed(2)  
+        };
+
+             
+        console.log(currentTown);
+    }    
+}
+
+towns(['Sofia | 42.696552 | 23.32601',
+'Beijing | 39.913818 | 116.363625']);
