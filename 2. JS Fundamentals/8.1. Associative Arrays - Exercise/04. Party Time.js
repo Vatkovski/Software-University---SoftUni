2489195c172aa9b7566d5b428284b657cd4734de
Vatkovski <yulian.vatkovski@gmail.com>
2023-09-12 23:28:41 +0300

    Add files via upload

---
+++ 2. JS Fundamentals/8.1. Associative Arrays - Exercise/04. Party Time.js
@@ -0,0 +1,40 @@
+function partyTime(input) {
+    let vipList = [];
+    let regularList = [];
+    let command = input.shift();
+
+    while (command !== 'PARTY') {
+        let firstChar = command[0];
+
+        if (isNaN(firstChar)) {
+            regularList.push(command)
+        } else {
+            vipList.push(command);
+        }
+
+        command = input.shift();
+    }
+
+    let allGuests = vipList.concat(regularList);
+
+    for (const guest of input) {
+        if (allGuests.includes(guest)) {
+            allGuests.splice(allGuests.indexOf(guest), 1);
+        }
+    }
+
+    console.log(allGuests.length);
+    allGuests.forEach(guest => console.log(guest));
+
+}
+
+partyTime(['7IK9Yo0h',
+           '9NoBUajQ',
+           'Ce8vwPmE',
+           'SVQXQCbc',
+           'tSzE5t0p',
+           'PARTY',
+           '9NoBUajQ',
+           'Ce8vwPmE',
+           'SVQXQCbc'
+       ]);
