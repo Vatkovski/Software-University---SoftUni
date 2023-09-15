be23095b74947d319e863aaf9469cd9b4e36d0c7
Vatkovski <yulian.vatkovski@gmail.com>
2023-09-15 13:16:33 +0300

    Add files via upload

---
+++ 2. JS Fundamentals/01.2. Basic Syntax, Conditional Statements and Loops - More Exercise/04. Reverse String.js
@@ -0,0 +1,7 @@
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+console.log(reverseString("Hello")); // Outputs: olleH
+console.log(reverseString("SoftUni")); // Outputs: inUtfoS
+console.log(reverseString("1234")); // Outputs: 4321
