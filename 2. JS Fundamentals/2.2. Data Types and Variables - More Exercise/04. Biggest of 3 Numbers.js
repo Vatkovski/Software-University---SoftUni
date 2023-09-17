fbbb655c569d33e580faa67ebcebc6f83e118306
Vatkovski <yulian.vatkovski@gmail.com>
2023-09-17 12:02:08 +0300

    Add files via upload

---
+++ 2. JS Fundamentals/2.2. Data Types and Variables - More Exercise/04. Biggest of 3 Numbers.js
@@ -0,0 +1,8 @@
+function findBiggestNumber(a, b, c) {
+    return Math.max(a, b, c);
+}
+
+console.log(findBiggestNumber(-2, 7, 3)); // Output: 7
+console.log(findBiggestNumber(130, 5, 99)); // Output: 130
+console.log(findBiggestNumber(43, 43.2, 43.1)); // Output: 43.2
+console.log(findBiggestNumber(2, 2, 2)); // Output: 2
