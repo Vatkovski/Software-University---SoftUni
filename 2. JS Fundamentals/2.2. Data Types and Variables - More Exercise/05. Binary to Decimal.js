736e2ac50a93100696336c51133b7c9ba31a7bc3
Vatkovski <yulian.vatkovski@gmail.com>
2023-09-18 09:40:29 +0300

    Add files via upload

---
+++ 2. JS Fundamentals/2.2. Data Types and Variables - More Exercise/05. Binary to Decimal.js
@@ -0,0 +1,6 @@
+function binaryToDecimal(binaryString) {
+    return parseInt(binaryString, 2);
+}
+
+console.log(binaryToDecimal('00001001')); // Output: 9
+console.log(binaryToDecimal('11110000')); // Output: 240
