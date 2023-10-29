b653e2fce973d75fb839662a404b79decf8e14bd
Yuliyan Vatkovski <yulian.vatkovski@gmail.com>
2023-10-29 11:22:00 +0200

    Add files via upload

---
+++ Programming Basics JavaScript/2. Conditional Statements - Lab/05. Number 100...200.js
@@ -0,0 +1,12 @@
+function checkNumber(num) {
+    if (num < 100) {
+        console.log("Less than 100");
+    } else if (num >= 100 && num <= 200) {
+        console.log("Between 100 and 200");
+    } else {
+        console.log("Greater than 200");
+    }
+}
+
+
+checkNumber(95); 
