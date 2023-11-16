125da4b5b736e2dbd586c45a3b21ac6db3a725b5
Yuliyan Vatkovski <yulian.vatkovski@gmail.com>
2023-11-16 10:00:32 +0200

    Add files via upload

---
+++ 3. JS Advanced/06. DOM Manipulations and Events/01. List-Of-Items/app.js
@@ -0,0 +1,10 @@
+function addItem() {
+    let text = document.getElementById('newItemText').value;
+    let li = document.createElement("li");
+    
+    li.appendChild(document.createTextNode(text));
+    
+    document.getElementById("items").appendChild(li);
+
+    document.getElementById("newItemText").value = '';
+}
