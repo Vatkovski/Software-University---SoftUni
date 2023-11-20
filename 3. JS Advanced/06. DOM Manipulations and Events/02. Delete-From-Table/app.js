64688246fd8c58afadca0e6b5ff651df3337ca31
Yuliyan Vatkovski <yulian.vatkovski@gmail.com>
2023-11-20 15:45:52 +0200

    Add files via upload

---
+++ 3. JS Advanced/06. DOM Manipulations and Events/02. Delete-From-Table/app.js
@@ -0,0 +1,14 @@
+function deleteByEmail() {
+    let email = document.getElementsByName("email")[0].value;
+    let secondColumn = document.querySelectorAll("#customers tr td:nth-child(2)");
+
+    for (const td of secondColumn) {
+        if (td.textContent == email) {
+            let row = td.parentNode;
+            row.parentNode.removeChild(row);
+            document.getElementById('result').textContent = "Deleted";
+            return;
+        }
+        document.getElementById('result').textContent = "Not found";
+    }
+}
