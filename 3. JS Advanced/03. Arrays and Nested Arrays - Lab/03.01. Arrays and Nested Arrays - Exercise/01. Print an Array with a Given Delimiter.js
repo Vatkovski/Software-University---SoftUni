26f70b31e065c697d57532aa9f3a0f15a67802b6
Yuliyan Vatkovski <yulian.vatkovski@gmail.com>
2023-12-19 13:19:53 +0200

    Delete 3. JS Advanced/03. Arrays and Nested Arrays - Lab/03.01. Arrays and Nested Arrays - Exercise directory

--- 3. JS Advanced/03. Arrays and Nested Arrays - Lab/03.01. Arrays and Nested Arrays - Exercise/01. Print an Array with a Given Delimiter.js
+++
@@ -1,6 +0,0 @@
-function printArrayWithDelimiter(array, delimiter) {
-    console.log(array.join(delimiter));
-}
-
-printArrayWithDelimiter(['One', 'Two', 'Three', 'Four', 'Five'], '-'); 
-// printArrayWithDelimiter(['How about no?', 'I', 'will', 'not', 'do', 'it!'], '_');
