addfbcd08ced9ab7c5270a90907a4e6516e0db2b
Yuliyan Vatkovski <yulian.vatkovski@gmail.com>
2023-12-21 22:51:34 +0200

    Add files via upload

---
+++ 3. JS Advanced/03.01. Arrays and Nested Arrays - Exercise/04. Rotate Array.js
@@ -0,0 +1,8 @@
+function rotateArray(array, rotations) {
+    for (let i = 0; i < rotations; i++) {
+        array.unshift(array.pop());
+    }
+    console.log(array.join(' '));
+}
+
+rotateArray(['1', '2', '3', '4'], 2);
