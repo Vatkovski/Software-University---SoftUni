2782c18c2bbf1aba30c990e525868bf4c7a1501a
Yuliyan Vatkovski <yulian.vatkovski@gmail.com>
2024-01-10 09:46:46 +0200

    Add files via upload

---
+++ 3. JS Advanced/03.03. Objects and Composition - Exercise/10. Heroes.js
@@ -0,0 +1,39 @@
+function solve() {
+    let heroes = {
+        mage: function (name) {
+            return {
+                name,
+                health: 100,
+                mana: 100,
+                cast: function (spell) {
+                    this.mana--;
+                    console.log(`${this.name} cast ${spell}`);
+                }
+            }
+        },
+        fighter: function (name) {
+            return {
+                name,
+                health: 100,
+                stamina: 100,
+                fight: function () {
+                    this.stamina--;
+                    console.log(`${this.name} slashes at the foe!`);
+                }
+            }
+        }
+    }
+    return heroes;
+}
+
+let create = solve();
+const scorcher = create.mage("Scorcher");
+scorcher.cast("fireball");
+scorcher.cast("thunder");
+scorcher.cast("light");
+
+const scorcher2 = create.fighter("Scorcher 2");
+scorcher2.fight();
+
+console.log(scorcher2.stamina);
+console.log(scorcher.mana);
