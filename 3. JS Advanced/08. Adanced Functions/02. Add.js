06e10aa53520d845604b9490a0bacde8ab8ed72f
Yuliyan Vatkovski <yulian.vatkovski@gmail.com>
2024-01-14 14:53:34 +0200

    Add files via upload

---
+++ 3. JS Advanced/08. Adanced Functions/02. Add.js
@@ -0,0 +1,14 @@
+function solution(number) {
+    return function(additionalNumber) {
+        return number + additionalNumber;
+    };
+}
+
+
+let add5 = solution(5);
+console.log(add5(2)); 
+console.log(add5(3));  
+
+// let add7 = solution(7);
+// console.log(add7(2));  
+// console.log(add7(3));  
