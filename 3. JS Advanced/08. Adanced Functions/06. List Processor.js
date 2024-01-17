baeb9134adf36b07463fb3bae494b9b73e1aecea
Yuliyan Vatkovski <yulian.vatkovski@gmail.com>
2024-01-17 10:21:28 +0200

    Add files via upload

---
+++ 3. JS Advanced/08. Adanced Functions/06. List Processor.js
@@ -0,0 +1,24 @@
+function listProcessor(input) {
+
+    const listCreator = () => {
+        let list = [];
+        return {
+            add: (el) => {
+                list.push(el);
+            },
+            remove: (el) => {
+                list = list.filter((x) => x !== el);
+            },
+            print: () => {
+                console.log(list.join(','));
+            },
+        };
+    };
+
+    const listHandler = listCreator();
+    input
+        .map((x) => x.split(' '))
+        .map(([cmd, text]) => listHandler[cmd](text));
+}
+
+console.log(listProcessor(['add hello', 'add again', 'remove hello', 'add again', 'print']));
