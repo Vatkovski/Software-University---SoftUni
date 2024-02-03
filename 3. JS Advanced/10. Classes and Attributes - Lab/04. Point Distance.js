7b7119412d19d9bab50605fa7b278044488528c2
Yuliyan Vatkovski <yulian.vatkovski@gmail.com>
2024-02-03 09:03:02 +0200

    Add files via upload

---
+++ 3. JS Advanced/10. Classes and Attributes - Lab/04. Point Distance.js
@@ -0,0 +1,16 @@
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    static distance(p1, p2) {
+        const dx = p1.x - p2.x;
+        const dy = p1.y - p2.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+}
+
+let p1 = new Point(5, 5);
+let p2 = new Point(9, 8);
+console.log(Point.distance(p1, p2)); 
