fda00ae4c93c00f70bc337345d0e9020538417d4
Yuliyan Vatkovski <yulian.vatkovski@gmail.com>
2024-02-14 11:55:23 +0200

    Add files via upload

---
+++ 3. JS Advanced/11.01. Prototypes and Inheritance - Exercise/07. Computer.js
@@ -0,0 +1,94 @@
+function createComputerHierarchy() {
+    class Keyboard {
+        constructor(manufacturer, responseTime) {
+            this.manufacturer = manufacturer;
+            this.responseTime = responseTime;
+        }
+    }
+
+    class Monitor {
+        constructor(manufacturer, width, height) {
+            this.manufacturer = manufacturer;
+            this.width = width;
+            this.height = height;
+        }
+    }
+
+    class Battery {
+        constructor(manufacturer, expectedLife) {
+            this.manufacturer = manufacturer;
+            this.expectedLife = expectedLife;
+        }
+    }
+
+    class Computer {
+        constructor(manufacturer, processorSpeed, ram, hardDiskSpace) {
+            if (new.target === Computer) {
+                throw new Error("Cannot instantiate an abstract class.");
+            }
+            this.manufacturer = manufacturer;
+            this.processorSpeed = processorSpeed;
+            this.ram = ram;
+            this.hardDiskSpace = hardDiskSpace;
+        }
+    }
+
+    class Laptop extends Computer {
+        constructor(manufacturer, processorSpeed, ram, hardDiskSpace, weight, color, battery) {
+            super(manufacturer, processorSpeed, ram, hardDiskSpace);
+            this.weight = weight;
+            this.color = color;
+            this._battery = battery;
+        }
+
+        get battery() {
+            return this._battery;
+        }
+
+        set battery(value) {
+            if (!(value instanceof Battery)) {
+                throw new TypeError("The passed-in argument is not an instance of Battery.");
+            }
+            this._battery = value;
+        }
+    }
+
+    class Desktop extends Computer {
+        constructor(manufacturer, processorSpeed, ram, hardDiskSpace, keyboard, monitor) {
+            super(manufacturer, processorSpeed, ram, hardDiskSpace);
+            this._keyboard = keyboard;
+            this._monitor = monitor;
+        }
+
+        get keyboard() {
+            return this._keyboard;
+        }
+
+        set keyboard(value) {
+            if (!(value instanceof Keyboard)) {
+                throw new TypeError("The passed-in argument is not an instance of Keyboard.");
+            }
+            this._keyboard = value;
+        }
+
+        get monitor() {
+            return this._monitor;
+        }
+
+        set monitor(value) {
+            if (!(value instanceof Monitor)) {
+                throw new TypeError("The passed-in argument is not an instance of Monitor.");
+            }
+            this._monitor = value;
+        }
+    }
+
+    return {
+        Battery,
+        Keyboard,
+        Monitor,
+        Computer,
+        Laptop,
+        Desktop
+    }
+}
